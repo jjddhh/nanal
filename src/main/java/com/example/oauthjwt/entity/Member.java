72dae2e4eda37cdcbc634760d9ede9fdfdb5bc25
ehdgh <ehdgh25@naver.com>
2022-11-07 16:21:07 +0900

    ec2 소셜로그인 테스트

--- src/main/java/com/example/oauthjwt/entity/Member.java
+++ src/main/java/com/example/oauthjwt/entity/Member.java
@@ -3,6 +3,7 @@ package com.example.oauthjwt.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.DayOfWeek;
 
 @Getter
 @Builder
@@ -12,12 +13,26 @@ import javax.persistence.*;
 public class Member {
 
     @Id @GeneratedValue
-    private Long id;
+    private Long memberId;
 
+    @Column(unique = true, nullable = false, length = 40)
+    private String email;
+
+    @Column(nullable = false, length = 10)
+    private String provider;
+
+    @Column(nullable = false, length = 10)
     private String name;
 
-    @Column(unique = true)
-    private String email;
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DayOfWeek retrospectDay;
+
+    @Column(nullable = false)
+    private Boolean resetAvail;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
@@ -26,11 +41,9 @@ public class Member {
     @Getter
     @RequiredArgsConstructor
     public enum Role {
-
         USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
 
         private final String key;
-
     }
 }
 
